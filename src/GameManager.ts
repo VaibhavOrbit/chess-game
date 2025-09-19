6ea746f2cc3341222d781742bb0348810c019a40
VaibhavOrbit <vaibhav.readme@gmail.com>
2025-09-19 20:56:47 +0530

    todo:- fix move

--- src/GameManager.ts
+++ src/GameManager.ts
@@ -1,6 +1,7 @@
 import { WebSocket } from "ws";
-import { INIT_GAME } from "./message";
+import { INIT_GAME, MOVE } from "./message";
 import { Game } from "./Game";
+import { Move } from "chess.js";
 
 export class GameManager {
     private games: Game[];
@@ -16,19 +17,15 @@ export class GameManager {
     addUser(socket: WebSocket){
         this.users.push(socket);
         this.addHandler(socket)
-    
     }
 
     removeUser(socket: WebSocket){
         this.users = this.users.filter(user => user!==socket);
-        // Stop the game here because the iser left=
     }
 
     private addHandler(socket: WebSocket){
         socket.on("message", (data) =>{
             const message = JSON.parse(data.toString());
-        
-
             if(message.type === INIT_GAME){
                if(this.pendingUser) {
                 const game = new Game(this.pendingUser, socket);
@@ -38,8 +35,17 @@ export class GameManager {
                  this.pendingUser = socket;
                }
             }
-        })
+            if(message.type === MOVE){
+                
+                const game = this.games.find(game => game.player1 === socket || game.player2 === socket);
+                if(game){
+                    game.makeMove(socket, message.move);
+                }
 
-    }
-    
+            }
+        })
+    }  
 }
+
+
+
--- src/message.ts
+++ src/message.ts
@@ -1,1 +1,3 @@
 export const INIT_GAME = 'init_game';
+export const MOVE = 'MOVE';
+export const GAME_OVER = "GAME_OVER"
